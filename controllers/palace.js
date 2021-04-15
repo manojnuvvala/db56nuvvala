f85440c59f646c473eafdcd23f453a98d28cff37
manojnuvvala <manoj.nuvvala@gmail.com>
2021-04-14 20:00:53 -0500

    files added

--- controllers/palace.js
+++ controllers/palace.js
@@ -15,8 +15,16 @@ exports.palace_list = async function(req, res) {
     }
     };
 // for a specific palace.
-exports.palace_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: palace detail: ' + req.params.id);
+exports.palace_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await palace.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+
 };
 // Handle palace create on POST.
 exports.palace_create_post = async function(req, res) {
@@ -39,13 +47,37 @@ exports.palace_create_post = async function(req, res) {
     };
 
 // Handle palace delete form on DELETE.
-exports.palace_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: palace delete DELETE ' + req.params.id);
+exports.palace_delete = async function(req, res) {
+    console.log("delete "  + req.params.id)
+    try {
+        result = await palace.findByIdAndDelete( req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
+
+
 // Handle palace update form on PUT.
-exports.palace_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: palace update PUT' + req.params.id);
+exports.palace_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await palace.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.palacename) toUpdate.palacename = req.body.palacename;
+        if(req.body.location) toUpdate.location = req.body.location;
+        if(req.body.constructed_year) toUpdate.constructed_year = req.body.constructed_year;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
 // VIEWS
 // Handle a show all view
 exports.palace_view_all_Page = async function(req, res) {
@@ -58,3 +90,67 @@ exports.palace_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // Handle a show one view with id specified by query
+exports.palace_view_one_Page = async function(req, res) {
+    console.log("single view for id "  + req.query.id)
+    try{
+        result = await palace.findById( req.query.id)
+        res.render('palacedetail', 
+{ title: 'palace Detail', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for creating a palace.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.palace_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('palacecreate', { title: 'palace Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a palace.
+// query provides the id
+exports.palace_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await palace.findById(req.query.id)
+        res.render('palaceupdate', { title: 'palace Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle a delete one view with id from query
+exports.palace_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await palace.findById(req.query.id)
+        res.render('palacedelete', { title: 'palace Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/palace.js
+++ routes/palace.js
@@ -4,5 +4,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', palace_controlers.palace_view_all_Page);
-
+router.get('/detail', palace_controlers.palace_view_one_Page);
+/* GET create palace page */
+router.get('/create', palace_controlers.palace_create_Page);
+/* GET create update page */
+router.get('/update', palace_controlers.palace_update_Page);
+/* GET create palace page */
+router.get('/delete', palace_controlers.palace_delete_Page);
 module.exports = router;
